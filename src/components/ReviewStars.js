5ace17f624770cb46f838cdf1f91a3b769ef0c0a
Kamsi-idimogu <90581167+Kamsi-idimogu@users.noreply.github.com>
2022-12-26 15:28:13 -0500

    add support for more review ratings

--- src/components/ReviewStars.js
+++ src/components/ReviewStars.js
@@ -4,14 +4,43 @@ import { BsStarFill, BsStarHalf, BsStar } from 'react-icons/bs';
 const ReviewStars = ({rating}) => {
     return (
         <div className='review-stars'>
-            <BsStarFill className='star'/>
-            <BsStarFill className='star'/>
-            <BsStarFill className='star'/>
-            <BsStarFill className='star'/>
-            { rating === 4.5 ?
+            { rating === 0.5 ?
+                <BsStarHalf className='star'/>
+                :
+                rating >= 1.0 ?
+                <BsStarFill className='star'/>
+                :
+                <BsStar className='star'/>   
+            }
+            { rating === 1.5 ?
+                <BsStarHalf className='star'/>
+                :
+                rating >= 2.0 ?
+                <BsStarFill className='star'/>
+                :
+                <BsStar className='star'/>   
+            }
+            { rating === 2.5 ?
+                <BsStarHalf className='star'/>
+                :
+                rating >= 3.0 ?
+                <BsStarFill className='star'/>
+                :
+                <BsStar className='star'/>   
+            }
+            { rating === 3.5 ?
+                <BsStarHalf className='star'/>
+                :
+                rating >= 4.0 ?
+                <BsStarFill className='star'/>
+                :
+                <BsStar className='star'/>   
+            }
+            {
+            rating === 4.5 ?
                 <BsStarHalf className='star'/>
                 : 
-                rating === 5.0 ?
+                rating >= 5.0 ?
                 <BsStarFill className='star'/>
                 :
                 <BsStar className='star'/>   
