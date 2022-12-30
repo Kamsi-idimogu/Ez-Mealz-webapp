c45eb715dbfe6b5541faa353115da6e2410596fb
Kamsi-idimogu <90581167+Kamsi-idimogu@users.noreply.github.com>
2022-12-29 21:37:08 -0500

    add remaining featured images to project

--- src/components/data/featured-data.js
+++ src/components/data/featured-data.js
@@ -1,11 +1,13 @@
-import featuredImg4 from '../../images/featured-image-4.png'
+import featuredImg1 from '../../images/featured-image-1.png'
+import featuredImg2 from '../../images/featured-image-2.png'
 import featuredImg3 from '../../images/featured-image-3.png'
+import featuredImg4 from '../../images/featured-image-4.png'
 
 const featured = [
     {
         id: 1,
         weburl: 'https://www.epicurious.com/recipes/food/views/ba-syn-chicken-and-coconut-rice-with-nuoc-cham-tomatoes-recipe#intcid=_epicurious-bottom-recirc_7a6ea602-5556-44b7-81cf-85a368f1e982_text2vec1',
-        image: 'https://assets.epicurious.com/photos/6367e51ad4ba5b9b8d1a4c3c/1:1/w_2240,c_limit/0913-chicken-nuoc-cham-tomatoes-lede.jpg',
+        image: featuredImg1,
         title: 'Chicken and Coconut Rice With Nuoc Cham Tomatoes',
         description: `Rich, coconutty, schmaltzy, bright, funky, and tangy, this skillet chicken dinner tantalizes the taste 
         buds on every level. Skin-on, bone-in chicken thighs are seasoned simply with salt and lots of pepper, 
@@ -19,7 +21,7 @@ const featured = [
     {
         id: 2,
         weburl: 'https://www.delish.com/cooking/recipe-ideas/a28776899/mac-and-chili-stew-recipe/',
-        image: 'https://edamam-product-images.s3.amazonaws.com/web-img/195/19576ce7d1d217cb4007fcc36f96cde9.jpg?X-Amz-Security-Token=IQoJb3JpZ2luX2VjECsaCXVzLWVhc3QtMSJHMEUCIQDMRgE60nHoJQgcIpHr365mAGLQQhgz3Ral%2Fp3U0Y2fywIgQIMUHjU3FlJ%2BacTlP%2BF09Cgt2I9IMjdtNTlm47gxe%2BQqzAQIYxAAGgwxODcwMTcxNTA5ODYiDGcw3XqdhpzoiaVX8CqpBIKS8bQf3WLzpn9pPEk8TI5jkmNvkciy1Mf%2BQYxSdW8T84KgjriUUbqFoaJncOUS5UXglsv0sm1KkyGcCVhTBS8eKPLTkcsj1NiyCRvvfIziyhVFhSunEWq2WB3hWfw7iHOkLkEql%2FDw%2FSXNJx%2B3pppMYd5RjD%2BB4CeWMsEkRFyd%2BRZs05Megtbblu3P%2BUQfotaohjk61%2B9YLoZt78bkFgjK1jPWzPzIOR%2FzhyissHJvFpUY6%2FKk29ykdR%2FK7il1jyLM48Y4%2Fn%2FPlUXK0NVA0FCANLT8tRVYBxxI0RGfn%2BIDOy4Y%2FtpimERTMXkUn5tP4CSLX81ZigitJBz44NiWEHKi9sMAJKi92Ht4%2BqsolGK9bJ6akbQ1ag6FqAAqH%2FIxXuxilcPHnrgJXZFzT6tvoHt3A057ERjCtWlSu3u3YOFq2ptUaQ50WXFlU2U8GICoupPVtK4GeyJn%2FofWFqKN0ZMqIZroZWIylNTwhMts%2F8RH1dtwE0GV3U8KpPQPekzWIAizVR1TWyj%2BbknVhNl4b4HPfzsO%2FEQF3Z5SBbAAiKgdioGyhOGdjya%2BxSX159Skpa2pz60gpHFWY6F1sLe3aCpj%2Bo9RE37i2nRG9lbnHGFQOiDqg%2BqeNDoORKUFArx7aBwuPamobI1szFgraOKtE9VwROSPMvIcimGTswgxbwKxQBPwpdVKPG508dYt3LhAAL6rG%2FtsY1QiaIP2TBM9z9FVTLrT7Oa27rcw3dSCnQY6qQF%2FQbMf54t15CXWWI1WH9qTlQ1dWx7r%2BSh7yysT57MZ8%2B5hrWAZhIgKBhoXHjNl4nlwDHJXTURVxKkvw0LANmuaR1QDaLGwIife4RthbvpSkZD8%2FY1IYNoQDFjdAVqrokzjVb1M%2FNEfMd4kQgCNGjBZsMhcwBJxsKRDlv1cREsqiQ4GFsMneMvNKYrjM71VbXqFoF3a5AIO85mDOZfa%2BZMkjh%2FGbCGVaDwO&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Date=20221219T185241Z&X-Amz-SignedHeaders=host&X-Amz-Expires=3600&X-Amz-Credential=ASIASXCYXIIFKUOULT4L%2F20221219%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Signature=d4488fedc5486436e7275c3cfcde246ed576a94249d1c904a3c73822170c7bd9',
+        image: featuredImg2,
         title: 'Mac & Chili Stew',
         description: `This is less a traditional stew and more of an amazing beef chili mixed with some macaroni and (lots) 
         of cheese. It's hearty comfort food at its finest and would make an excellent addition to any tailgating 
